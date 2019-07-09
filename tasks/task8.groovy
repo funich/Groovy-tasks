b824372bc92ff5fff7f7e1d72979cf058221a95c
Antoś Bućko <anton_butsko@epam.com>
2019-07-09 14:02:12 +0300

    Return imports

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,3 +1,7 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+
 def String parseAndFilterJson(String string) {
     def jsonSlurper = new JsonSlurper()
     def map_json = jsonSlurper.parseText(string)
