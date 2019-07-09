db83e08f443b15c51965ce33d7c0661671465da2
Antoś Bućko <anton_butsko@epam.com>
2019-07-09 16:26:15 +0300

    DigitSum is a function

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,14 +1,14 @@
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
+def digitsum(number) {
+    return Integer.toString(number, 10).collect { Integer.parseInt(it, 10) }.sum()
+}
 
 def String parseAndFilterJson(String string) {
     def jsonSlurper = new JsonSlurper()
     def map_json = jsonSlurper.parseText(string)
     def result_map = [:]
-    def digitsum = { number ->
-        a = Integer.toString(number, 10).collect { Integer.parseInt(it, 10) }.sum()
-    }
     map_json.each { name, age ->
         if (digitsum(age) == 9) {
             result_map.put(name, age)
