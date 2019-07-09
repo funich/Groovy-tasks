310846f5ceb254d5ba341c38b7bd9779026fef21
Uladzislau Zubtsou <uladzislau_zubtsou@epam.com>
2019-07-09 18:04:16 +0300

    Second task fix

--- tasks/task2.groovy
+++ tasks/task2.groovy
@@ -1,7 +1,12 @@
 
-def mulEvenOdd(list){
-    list.eachWithIndex { number, int i -> }
-        if (i % 2 == 0){ number *= 2 }
-        else { number *= 3}
+def mulEvenOdd(list) {
+    list.eachWithIndex{ number, i, index = i + 1 ->
+        if (i % 2 == 0) {
+            list[i] *= 2
+        } else {
+            list[i] *= 3
+        }
     }
+    return list
+}
 return this
