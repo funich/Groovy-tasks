e972e2f238a79df5abed31150de6af066b58b6b3
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-07-09 17:20:35 +0300

    task1v2

--- tasks/task1.groovy
+++ tasks/task1.groovy
@@ -1,6 +1,7 @@
 //task1.groovy - function int sum(int, int), summ 2 numbers. Example - sum(1,4) == 5
 
-int summ(int a, int b) {
-    summ = a + b
+def sum(int a, int b) {
+    return a + b
 }
-print(summ(1, 4))
+
+return this
