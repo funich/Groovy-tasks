725b8efd7f0b7bfada8608ae11765781126e018d
Antoś Bućko <anton_butsko@epam.com>
2019-07-09 12:19:05 +0300

    Add task4

---
+++ tasks/task4.groovy
@@ -0,0 +1,11 @@
+def int fib(int number) {
+    if (number == 0) {
+        return 0
+    } else if (number == 1) {
+        return 1
+    } else {
+        return fib(number - 1) + fib(number - 2)
+    }
+}
+
+return this
