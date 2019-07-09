6e9d6889d6913b56c31d168f2ec94107354c1148
Kiryl Kaminski <kaminsky-kirill@tut.by>
2019-07-09 17:15:46 +0300

    task8

---
+++ tasks/task8.groovy
@@ -0,0 +1,18 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+def parseAndFilterJson(String text) {
+    def jsonSlurper = new JsonSlurper()
+    def parsing = jsonSlurper.parseText(text)
+    def res = [:]
+    parsing.each { name, years ->
+        if (years.intdiv(10) + years % 10 == 9) {
+            res.put(name, years)
+        }
+
+        return JsonOutput.toJson(res)
+    }
+}
+return this
+
+
