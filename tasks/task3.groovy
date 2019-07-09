26eceddfb9ff48fdbb81fb4269b398551599e645
margarita-markova <Margarita_Markova@epam.com>
2019-07-09 16:02:51 +0300

    4 try

--- tasks/task3.groovy
+++ tasks/task3.groovy
@@ -1,9 +1,17 @@
 def isSublist(def list, def sublist) {
-    def res = list.intersect(sublist)
-    if (res == sublist)
-        return true
-    else
-        return false
+    def ind, ind1
+    for (def it : sublist) {
+        if (ind == null) {
+            ind = list.indexOf(it)
+        }
+        else {
+            ind1 = list.indexOf(it)
+            if (ind != ind1 - 1 || ind != -1)
+                return false
+            ind = ind1
+        }
+    }
+    return true
 }
 
 return this
