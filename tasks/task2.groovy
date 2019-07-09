fb4f4f6b5c5123efa108d7545eda97cb8ac439f8
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-07-09 16:10:15 +0300

    Add files via upload

---
+++ tasks/task2.groovy
@@ -0,0 +1,12 @@
+def mulEvenOdd(array) {
+    for (int i = 0; i < array.size(); i++) {
+        if (array[i] % 2 == 0) {
+            array[i] *= 2
+        }
+        else {
+            array[i] *= 3
+        }
+    }
+    return array
+}
+return this
