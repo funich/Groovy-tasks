71b64ea319cb04b0eee2a2186d72ea55a9d1bb66
Kiryl Kaminski <kaminsky-kirill@tut.by>
2019-07-09 18:52:54 +0300

    task11 modified3

--- tasks/task11.groovy
+++ tasks/task11.groovy
@@ -1,14 +1,12 @@
-something = "1() 2() 3()"
-
 def gstring(String string, int num) {
     done =''
    for (count in 1..3) {
        w = ''
        if (count == num) {
            w = string
-           done += "${count}(${w}) "
        }
+       done += "${count}(${w}) "
    }
     return done
 }
-return this
+print gstring("test", 1)
