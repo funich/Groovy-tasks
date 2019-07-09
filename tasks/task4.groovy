dd5bd9623f79abc7b06736816351deac830acc8a
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-07-09 17:37:59 +0300

    Add files via upload

---
+++ tasks/task4.groovy
@@ -0,0 +1,17 @@
+def int fib (int num){
+    if (num == 0) {
+        return 0
+    }
+    else if (num == 1) {
+        return 1
+    }
+    else if (num == 2) {
+        return 1
+    }
+    else {
+        return fib(num - 1) + fib(num - 2)
+    }
+
+}
+return this
+
