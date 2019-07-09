558bf5742c9f2c7d43fd99b6950bf6a29a6ddd5d
Vitali Ulantsau <ssvlay@gmail.com>
2019-07-09 15:23:06 +0300

    Create task8.groovy

---
+++ tasks/task8.groovy
@@ -0,0 +1,21 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+
+def String parseAndFilterJson(String string) {
+    def jsonSlurper = new JsonSlurper()
+    def map_json = jsonSlurper.parseText(string)
+    def result_map = [:]
+    def digitsum = { number ->
+        a = Integer.toString(number, 10).collect { Integer.parseInt(it, 10) }.sum()
+    }
+    map_json.each { name, age ->
+        if (digitsum(age) == 9) {
+            result_map.put(name, age)
+        }
+    }
+
+    return JsonOutput.toJson(result_map)
+}
+
+return this
