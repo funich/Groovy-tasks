11c786d27ebc66e33f3fc3499d714bf27a4d9083
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-07-09 15:01:24 +0300

    Changed task8

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,22 +1,18 @@
 import groovy.json.JsonSlurper;
 import groovy.json.JsonOutput;
 
-jsonSlurper = new JsonSlurper()
-
 def parseAndFilterJson(String str){
 
+    def jsonSlurper = new JsonSlurper()
     def list = jsonSlurper.parseText(str)
 
     def approved = [:]
     list.each { key, value ->
-        def dig_sum =  Integer.toString(value).collect{ Integer.parseInt(it) }.sum()
-        if(dig_sum == 9) approved.put(key, value)
+        if((value%10)+(value.intdiv(10)) == 9) approved.put(key, value)
     }
 
     return JsonOutput.toJson(approved)
 
 }
-
-//println(parseAndFilterJson("{\"Kate\":18,\"Alan\":16,\"Osvald\":27}"))
 
 return this
